464bc79f9e078bd9ba18ae5683ad2c70cdae85d5
Oleg Natalushko <onatalushko@adyax.com>
2018-03-29 19:10:57 +0300

    fixes

--- js/commerce_add_to_cart_confirmation.js
+++ js/commerce_add_to_cart_confirmation.js
@@ -1,14 +1,14 @@
-(function ($, Drupal, drupalSettings) {
+(function ($, Drupal) {
 
   'use strict';
 
   Drupal.behaviors.commerce_add_to_cart_confirmation_overlay = {
-    attach:function (context, settings) {
-      var popup_content = $('.messages--commerce-add-to-cart-confirmation');
+    attach:function (context) {
+      var popup_content = $('.messages--commerce-add-to-cart-confirmation > div', context).clone();
+      $('.messages--commerce-add-to-cart-confirmation').remove();
       if (popup_content.length > 0) {
         var popup_title = popup_content.find('.added-product-title').html();
         popup_content.find('.added-product-title').remove();
-        $('.messages--commerce-add-to-cart-confirmation').remove();
 
         var confirmationModal = Drupal.dialog(popup_content, {
           title: popup_title,
@@ -24,17 +24,17 @@
           },
           buttons: [
             {
-              text: Drupal.t('Go to checkout'),
-              class: 'button button--primary',
+              text: Drupal.t('Continue shopping'),
+              class: 'button',
               click: function () {
-                window.location.href = '/cart';
                 confirmationModal.close();
               }
             },
             {
-              text: Drupal.t('Continue shopping'),
-              class: 'button',
+              text: Drupal.t('Go to checkout'),
+              class: 'button button--primary',
               click: function () {
+                window.location.href = '/cart';
                 confirmationModal.close();
               }
             }
@@ -44,4 +44,4 @@
       }
     }
   }
-})(jQuery, Drupal, drupalSettings);
+})(jQuery, Drupal);
